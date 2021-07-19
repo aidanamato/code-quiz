f43688a07298ba4b082b0655870021b92c049ffe
Aidan Amato <aidanamato@comcast.net>
2021-07-18 22:54:58 -0400

    question bank complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,35 @@ var questionIterator = 0;
 
 // question bank with innerHTML
 var questionBankArr = [
-  // Which of these variables is an array? 1. var arr = (a, b, c, d), 2. var arr = [a, b, c, d], 3. var arr = {a, b, c, d}, 4. var arr = <a, b, c, d>
+  // Which of these variables is an array? 1. var arr = (a, b, c, d), 2. var arr = [a, b, c, d] (correct), 3. var arr = {a, b, c, d}, 4. var arr = &lta, b, c, d&gt
   '<div class="question"><h3>Which of these variables is an array?</h3><ol class="answers"><li><button type="button" class="btn-primary incorrect"><p>1. var arr = ("a", "b", "c", "d")</p></button></li><li><button type="button" class="btn-primary correct"><p>2. var arr = ["a", "b", "c", "d"]</p></button></li><li><button type="button" class="btn-primary incorrect"><p>3. var arr = {"a", "b", "c", "d"}</p></button></li><li><button type="button" class="btn-primary incorrect"><p>4. var arr = &lt"a", "b", "c", "d"&gt</p></button></li></ol></div>',
-  // Which of these values is <b>not<b> a truthy value? 1. "false", 2. [], 3. {}, 4. ""
+
+  // Which of these values is <b>not<b> a truthy value? 1. "false", 2. [], 3. {}, 4. "" (correct)
   '<div class="question"><h3>Which of these values is <b>not</b> a truthy value?</h3><ol class="answers"><li><button type="button" class="btn-primary incorrect"><p>1. "false"</p></button></li><li><button type="button" class="btn-primary incorrect"><p>2. []</p></button></li><li><button type="button" class="btn-primary incorrect"><p>3. {}</p></button></li><li><button type="button" class="btn-primary correct"><p>4. ""</p></button></li></ol></div>',
-  // What is the shorthand used in Javascript to increase a number by 1? 1. ++, 2. +1, 3. 1+, 4. +
+
+  // What is the shorthand used in Javascript to increase a number by 1? 1. ++ (correct), 2. +1, 3. 1+, 4. +
   '<div class="question"><h3>What is the shorthand used in Javascript to increase a number by 1?</h3><ol class="answers"><li><button type="button" class="btn-primary correct"><p>1. ++</p></button></li><li><button type="button" class="btn-primary incorrect"><p>2. +1</p></button></li><li><button type="button" class="btn-primary incorrect"><p>3. 1+</p></button></li><li><button type="button" class="btn-primary incorrect"><p>4. +</p></button></li></ol></div>',
-  // What built in JavaScript object must you access in order to interact with the DOM (Document Object Model)? 1. dom, 2. document, 3. event, 4. jQuery
+
+  // What built in JavaScript object must you access in order to interact with the DOM (Document Object Model)? 1. dom, 2. document (correct), 3. event, 4. jQuery
   '<div class="question"><h3>What built in JavaScript object must you access in order to interact with the DOM (Document Object Model)?</h3><ol class="answers"><li><button type="button" class="btn-primary incorrect"><p>1. dom</p></button></li><li><button type="button" class="btn-primary correct"><p>2. document</p></button></li><li><button type="button" class="btn-primary incorrect"><p>3. event</p></button></li><li><button type="button" class="btn-primary incorrect"><p>4. jQuery</p></button></li></ol></div>',
-  // What is the file extension used for Javascript files? 1. .js, 2. .jv, 3. .java, 4. .script
+  
+  // What is the file extension used for Javascript files? 1. .js (correct), 2. .jv, 3. .java, 4. .script
   '<div class="question"><h3>What is the file extension used for Javascript files?</h3><ol class="answers"><li><button type="button" class="btn-primary correct"><p>1. .js</p></button></li><li><button type="button" class="btn-primary incorrect"><p>2. .jv</p></button></li><li><button type="button" class="btn-primary incorrect"><p>3.  .java</p></button></li><li><button type="button" class="btn-primary incorrect"><p>4. .script</p></button></li></ol></div>',
-  // Which built in Window method is used to repeatedly call a function after a certain amount of time? 1. setTimer(), 2. setRepeat(), 3. setInterval(), 4. setContinual()
+
+  // Which built in Window method is used to repeatedly call a function after a certain amount of time? 1. setTimer(), 2. setRepeat(), 3. setInterval() (correct), 4. setContinual()
   '<div class="question"><h3>Which built in Window method is used to repeatedly call a function after a certain amount of time?</h3><ol class="answers"><li><button type="button" class="btn-primary incorrect"><p>1. setTimer()</p></button></li><li><button type="button" class="btn-primary incorrect"><p>2. setRepeat()</p></button></li><li><button type="button" class="btn-primary correct"><p>3. setInterval()</p></button></li><li><button type="button" class="btn-primary incorrect"><p>4. setContinual()</p></button></li></ol></div>',
+  
+  // Which of these for loop conditional statements will result in the code block running exactly 6 times? 1. for (var i=0; i<6; i = i+2) {}, 2. for (var i=10; i>=5; i--) {} (correct), 3. for (var i=1; i=7; i++) {}, 4. for (var i=0; i<6; i--) {}
+  '<div class="question"><h3>Which of these for loop conditional statements will result in the code block running exactly 6 times?</h3><ol class="answers"><li><button type="button" class="btn-primary incorrect"><p>1. for (var i=0; i<6; i = i+2) {}</p></button></li><li><button type="button" class="btn-primary correct"><p>2. for (var i=10; i>=5; i--) {}</p></button></li><li><button type="button" class="btn-primary incorrect"><p>3. for (var i=1; i=7; i++) {}</p></button></li><li><button type="button" class="btn-primary incorrect"><p>4. for (var i=0; i<6; i--) {}</p></button></li></ol></div>',
+  
+  // Which of these query selectors would return the first element with a class of "primary"? 1. document.querySelector(.primary), 2. document.querySelector("primary"), 3. document.querySelector("#primary"), 4. document.querySelector(".primary") (correct)
+  '<div class="question"><h3>Which of these query selectors would return the first element with a class of "primary"?</h3><ol class="answers"><li><button type="button" class="btn-primary incorrect"><p>1. document.querySelector(.primary)</p></button></li><li><button type="button" class="btn-primary incorrect"><p>2. document.querySelector("primary")</p></button></li><li><button type="button" class="btn-primary incorrect"><p>3. document.querySelector("#primary")</p></button></li><li><button type="button" class="btn-primary correct"><p>4. document.querySelector(".primary")</p></button></li></ol></div>',
+  
+  // Which of these expressions sets a class of "hero" to the &ltsection&gt element? 1. document.querySelector("section").className = "hero" (correct), 2. document.querySelector("section").addClass = "hero", 3. document.querySelector("section").class = "hero", 4. document.querySelector("section").addClassName = "hero"
+  '<div class="question"><h3>Which of these expressions sets a class of "hero" to the &ltsection&gt element?</h3><ol class="answers"><li><button type="button" class="btn-primary correct"><p>1. document.querySelector("section").className = "hero"</p></button></li><li><button type="button" class="btn-primary incorrect"><p>2. document.querySelector("section").addClass = "hero"</p></button></li><li><button type="button" class="btn-primary incorrect"><p>3. document.querySelector("section").class = "hero"</p></button></li><li><button type="button" class="btn-primary incorrect"><p>4. document.querySelector("section").addClassName = "hero"</p></button></li></ol></div>',
+  
+  // To which object does the "alert()" method belong? 1. browser, 2. console, 3. window (correct), 4. document
+  '<div class="question"><h3>To which object does the "alert()" method belong?</h3><ol class="answers"><li><button type="button" class="btn-primary incorrect"><p>1. browser</p></button></li><li><button type="button" class="btn-primary incorrect"><p>2. console</p></button></li><li><button type="button" class="btn-primary correct"><p>3. window</p></button></li><li><button type="button" class="btn-primary incorrect"><p>4. document</p></button></li></ol></div>'
 ];
 
 // quiz timer function
